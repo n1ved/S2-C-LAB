05988a974f489a210808469905b6a710f3d6186b
n1ved <nivedrsalini@gmail.com>
2023-04-07 20:06:59 +0530

    D-1.5 Added sum.c

---
+++ sum.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main(){
+	int a , b , sum ;
+	printf("Enter two number : ");
+	scanf("%d  %d" , &a , &b);
+	sum = a + b ;
+	printf("Sum of %d and %d is %d\n" , a , b ,sum);
+}
