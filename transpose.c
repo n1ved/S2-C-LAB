4b1d97072fb655e1fd88c75d2aa1d549a03e259f
n1ved <nivedrsalini@gmail.com>
2023-07-25 19:03:52 +0530

    Transpose of matrix added

---
+++ transpose.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void main(){
+  int n,m;
+  scanf("%d%d",&n,&m);
+  int a[n][m] , b[m][n];
+
+  for(int i=0 ; i<n ; i++){
+    for  (int j=0 ; j<m ; j++){
+      scanf("%d",&a[i][j]);
+    }
+  }
+
+  for(int i=0 ; i<n ; i++){
+    for(int j=0 ; j<m ; j++){
+      b[j][i] = a[i][j];
+    }
+  }
+
+  for(int i=0 ; i<m ; i++){
+    for(int j=0 ; j<n ; j++){
+      printf("%d\t", b[i][j]);
+    }
+    printf("\n");
+  }
+
+}
