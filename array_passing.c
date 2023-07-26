74e5f84c129bf2ddad0c289318e63c750b7c6776
n1ved <nivedrsalini@gmail.com>
2023-07-26 19:02:16 +0530

    Passing array to a function

---
+++ array_passing.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void printArray(int arr[] ,int n){
+  for(int i=0; i<n ;i++){
+    printf("%d" , arr[i]);
+  }
+}
+
+void main(){
+  int n;
+  scanf("%d",&n);
+  int arr[n];
+  for(int i=0 ; i<n ; i++){
+    scanf("%d",&arr[i]);
+  }
+
+  printArray(arr , n);
+}
